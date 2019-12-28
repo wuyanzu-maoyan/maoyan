98c52107153d8b166ccd19583ccfb59256e21c9a
zss <1750592414@qq.com>
2019-12-28 15:18:20 +0800

    登录头部

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
-  <div>
-    <Login></Login>
-  </div>
+  
 </template>
 
 <script type="text/ecmascript-6">
   import Login from './pages/Login/Login';
   export default {
-    components:{
-      Login
-    }
+    
   }
 </script>
 
--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -3,9 +3,14 @@
     <div>头部组件</div>
     <div class="loginInner">
       <div class="loginHeader">
-        <a href="javascript:;" >短信登录</a>
-        <a href="javascript:;" >密码登录</a>  
+        <div class='active'>
+          <a href="javascript:;" >美团账号登录</a>
+        </div>
+        <div>
+          <a href="javascript:;" >手机验证登录</a>
+        </div>    
       </div>
+      <div class="line"></div>
 
     </div>
   </div>
@@ -17,9 +22,30 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '../../common/stylus/mixins.styl';
   .loginInner
     .loginHeader
-      width 10vh
+      width 90vw
+      padding 0 5vw
       display flex
+      div
+        width 100%
+        height 72px
+        line-height 72px
+        text-align center
+        position relative
+        &.active::after
+          position absolute
+          width 100%
+          height 8px
+          content ''
+          display table
+          background-color #df2d2d
+    .line
+      width 100vw
+      height 8px
+      background-color #f8f8f8
+        
+
  
 </style>
