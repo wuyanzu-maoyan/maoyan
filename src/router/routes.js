dbce4595dc5e282f27ead839eb86029f035ac01c
kangjiachang <1004550976@qq.com>
2019-12-28 20:55:36 +0800

    update

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,13 @@
 
 //引入路由并注册 例如
 import Cinema from '../pages/Cinema/Cinema';
-import Home from '../pages/Home/Home';
-import Personal from '../pages/Personal/Personal';
-import City from '../pages/City/City';
-import CinemaDetail from '../pages/CinemaDetail/CinemaDetail';
-import Login from '../pages/CineDetail/CineDetail';
-import Now from '../pages/Home/Now/Now';
-import Will from '../pages/Home/Will/Will';
+// import Home from '../pages/Home/Home';
+// import Personal from '../pages/Personal/Personal';
+// import City from '../pages/City/City';
+// import CinemaDetail from '../pages/CinemaDetail/CinemaDetail';
+import Login from '../pages/Login/Login.vue';
+// import Now from '../pages/Home/Now/Now';
+// import Will from '../pages/Home/Will/Will';
 
 
 export default [
@@ -21,50 +21,50 @@ export default [
     path:"/cinema",
     component: Cinema
   },
-  {
-    path:"/home",
-    component: Home
-  },
-  {
-    path:"/personal",
-    component: Personal
-  },
-  {
-    path:"/city",
-    component: City
-  },
-  {
-    path:"/cinemadetail",
-    component: CinemaDetail
-  },
-  {
-    path:"/home",
-    component: Home,
-    children:[
-      {
-        path:'/home/now',
-        component: Now
-      },
-      {
-        path:'/home/Will',
-        component: Will
-      }
-    ]
-  },
-  {
-    path:"/personal",
-   component: Personal
-  },
-  {
-    path:"/city",
-    component: City
-  },
-  {
-    path:"/cinemadetail",
-    component: CinemaDetail
-  },
-  {
-    path:'/',
-    redirect:'/home'
-  }
+  // {
+  //   path:"/home",
+  //   component: Home
+  // },
+  // {
+  //   path:"/personal",
+  //   component: Personal
+  // },
+  // {
+  //   path:"/city",
+  //   component: City
+  // },
+  // {
+  //   path:"/cinemadetail",
+  //   component: CinemaDetail
+  // },
+  // {
+  //   path:"/home",
+  //   component: Home,
+  //   children:[
+  //     {
+  //       path:'/home/now',
+  //       component: Now
+  //     },
+  //     {
+  //       path:'/home/Will',
+  //       component: Will
+  //     }
+  //   ]
+  // },
+  // {
+  //   path:"/personal",
+  //  component: Personal
+  // },
+  // {
+  //   path:"/city",
+  //   component: City
+  // },
+  // {
+  //   path:"/cinemadetail",
+  //   component: CinemaDetail
+  // },
+  // {
+  //   path:'/',
+  //   redirect:'/home'
+  // }
 ]
