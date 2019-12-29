c544fedb10ec4a061f887d07dfb11beb7e05cedf
zss <1750592414@qq.com>
2019-12-30 00:02:04 +0800

    用户名登录的简单验证及滑块

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
-import { Button } from 'mint-ui';
+import { Button} from 'mint-ui';
+import VeeValidate from 'vee-validate'
+import 'lib-flexible' // flexible会自动根据设备情况动态设置rem的值的大小
 
 import App from './App.vue'
 import store from '@/vuex/store.js'
 
 import Header from '@/components/Header/Header.vue'
-import 'lib-flexible' // flexible会自动根据设备情况动态设置rem的值的大小
+
 import router from '@/router'
 
+// 声明使用vue插件
+Vue.use(VeeValidate)
 Vue.component('mint-button', Button);
 Vue.component('Header',Header)
 Vue.config.productionTip = false
