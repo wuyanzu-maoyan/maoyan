d2c3f340f0fa94f8bcc3d0c43651613b6ca05994
zss <1750592414@qq.com>
2019-12-30 11:40:41 +0800

    滑块验证

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import { Button} from 'mint-ui';
 import VeeValidate from 'vee-validate'
 import 'lib-flexible' // flexible会自动根据设备情况动态设置rem的值的大小
-
+import {VerifySlider} from 'vue-verify-slider';
 import App from './App.vue'
 import 'lib-flexible'
 import store from '@/vuex/store.js'
@@ -13,6 +13,8 @@ import router from '@/router'
 
 // 声明使用vue插件
 Vue.use(VeeValidate)
+//滑块验证
+Vue.component('VerifySlider',VerifySlider)
 Vue.component('mint-button', Button);
 Vue.component('Header',Header)
 Vue.config.productionTip = false
