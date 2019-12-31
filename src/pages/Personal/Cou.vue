4d4f1a735bf6364e250f8c23349e1ce217816b65
AJing <472953395@qq.com>
2019-12-31 13:41:29 +0800

    wj

--- src/pages/Personal/Cou.vue
+++ src/pages/Personal/Cou.vue
@@ -14,7 +14,93 @@
           </div>
         </div> 
         <div class="wjCouItemRight">
-          <div class="wjtitle">【电影】-新客专享券</div>
+          <div class="wjTitle">【电影】-新客专享券</div>
+          <div class="wjDescribe">满30元可用，限猫眼电影新用户使用，不可与其他优惠同享。 购票时，请在“活动与抵用券”栏选择该券</div>
+          <div class="wjLife">有效期 2019-10-26 至 2019-10-30</div>
+        </div>
+      </div>
+      <div class=wjCouItem>
+        <div class="wjCouItemLeft">
+          <div class="wjLeftWrapper">
+            <div class="wjAmount">10<span>元</span>
+            </div>
+            <div class="wjVoucher">代金券</div>
+          </div>
+        </div> 
+        <div class="wjCouItemRight">
+          <div class="wjTitle">【电影】-新客专享券</div>
+          <div class="wjDescribe">满30元可用，限猫眼电影新用户使用，不可与其他优惠同享。 购票时，请在“活动与抵用券”栏选择该券</div>
+          <div class="wjLife">有效期 2019-10-26 至 2019-10-30</div>
+        </div>
+      </div>
+      <div class=wjCouItem>
+        <div class="wjCouItemLeft">
+          <div class="wjLeftWrapper">
+            <div class="wjAmount">10<span>元</span>
+            </div>
+            <div class="wjVoucher">代金券</div>
+          </div>
+        </div> 
+        <div class="wjCouItemRight">
+          <div class="wjTitle">【电影】-新客专享券</div>
+          <div class="wjDescribe">满30元可用，限猫眼电影新用户使用，不可与其他优惠同享。 购票时，请在“活动与抵用券”栏选择该券</div>
+          <div class="wjLife">有效期 2019-10-26 至 2019-10-30</div>
+        </div>
+      </div>
+      <div class=wjCouItem>
+        <div class="wjCouItemLeft">
+          <div class="wjLeftWrapper">
+            <div class="wjAmount">10<span>元</span>
+            </div>
+            <div class="wjVoucher">代金券</div>
+          </div>
+        </div> 
+        <div class="wjCouItemRight">
+          <div class="wjTitle">【电影】-新客专享券</div>
+          <div class="wjDescribe">满30元可用，限猫眼电影新用户使用，不可与其他优惠同享。 购票时，请在“活动与抵用券”栏选择该券</div>
+          <div class="wjLife">有效期 2019-10-26 至 2019-10-30</div>
+        </div>
+      </div>
+      <div class=wjCouItem>
+        <div class="wjCouItemLeft">
+          <div class="wjLeftWrapper">
+            <div class="wjAmount">10<span>元</span>
+            </div>
+            <div class="wjVoucher">代金券</div>
+          </div>
+        </div> 
+        <div class="wjCouItemRight">
+          <div class="wjTitle">【电影】-新客专享券</div>
+          <div class="wjDescribe">满30元可用，限猫眼电影新用户使用，不可与其他优惠同享。 购票时，请在“活动与抵用券”栏选择该券</div>
+          <div class="wjLife">有效期 2019-10-26 至 2019-10-30</div>
+        </div>
+      </div>
+      <div class=wjCouItem>
+        <div class="wjCouItemLeft">
+          <div class="wjLeftWrapper">
+            <div class="wjAmount">10<span>元</span>
+            </div>
+            <div class="wjVoucher">代金券</div>
+          </div>
+        </div> 
+        <div class="wjCouItemRight">
+          <div class="wjTitle">【电影】-新客专享券</div>
+          <div class="wjDescribe">满30元可用，限猫眼电影新用户使用，不可与其他优惠同享。 购票时，请在“活动与抵用券”栏选择该券</div>
+          <div class="wjLife">有效期 2019-10-26 至 2019-10-30</div>
+        </div>
+      </div>
+      <div class=wjCouItem>
+        <div class="wjCouItemLeft">
+          <div class="wjLeftWrapper">
+            <div class="wjAmount">10<span>元</span>
+            </div>
+            <div class="wjVoucher">代金券</div>
+          </div>
+        </div> 
+        <div class="wjCouItemRight">
+          <div class="wjTitle">【电影】-新客专享券</div>
+          <div class="wjDescribe">满30元可用，限猫眼电影新用户使用，不可与其他优惠同享。 购票时，请在“活动与抵用券”栏选择该券</div>
+          <div class="wjLife">有效期 2019-10-26 至 2019-10-30</div>
         </div>
       </div>
     </div>
@@ -27,6 +113,8 @@
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixins.styl';
+
   .wjCoupon
     width 100%
     height 40px
@@ -56,6 +144,10 @@
       display flex
       justify-content stretch
       overflow hidden
+      background-image url(../../static/images/wjOut.png)
+      background-repeat no-repeat
+      background-size 65px
+      background-position top 7px right 7px
       .wjCouItemLeft
         width 23%
         height 100%
@@ -65,7 +157,6 @@
           width 100%
           height 70px
           margin-top 35px
-          // background-color red
           .wjAmount
             font-size 28px
             color white
@@ -80,6 +171,33 @@
       .wjCouItemRight
         width 77%
         height 100%
+        border-left 1px dashed rgb(195,195,195) 
+        .wjTitle
+          width 100%
+          height 24px
+          margin-top 10px
+          padding 0 16px
+          font-size 16px
+          line-height 24px
+        .wjDescribe
+          height 40px
+          margin 6px 0 43px 0
+          padding 0 16px
+          font-size 13px
+          line-height 14px
+          color rgb(153,153,153)
+        .wjLife
+          width 97%
+          height 22px
+          padding 0 4px
+          line-height 22px
+          text-align right
+          color rgb(119,119,119)
+          top-border-1px(#E1E1E1)
+          
+
+
+
 
 
         
