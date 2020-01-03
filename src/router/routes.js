9b616e5fa2e00f314f2b36c0ff69399aa41c4533
zhangyahui <1733594272@qq.com>
2020-01-03 17:23:54 +0800

    路由传参

--- src/router/routes.js
+++ src/router/routes.js
@@ -48,7 +48,7 @@ export default [
     component:FindPassword,
   },
   {
-    path:'/seats',
+    path:'/seats/:id',
     component:Seats,
     meta:{
       title:'中影星美国际影城（温都水城店）',//传给header组件的title
